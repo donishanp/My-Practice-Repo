8f04c87cc30f6527fea9a2c7f7b0710ba77322ae
donishanp <donishanp@gmail.com>
2023-02-17 04:58:45 +0000

    just more practice

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#Script:                           Ops 201 Class 02 Ops Challenge Solution
+#Author:                           Donisha Potts
+#Date of latest version:           02/16/23
+#Purpose:                          Print a string to the terminal
+
+
+#Main
+
+echo "I am working perfectly"
+
+
+
+#End
